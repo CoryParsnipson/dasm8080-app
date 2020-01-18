660ce717fce6647099f4a41db66ef22a64e0423c
Cory Parsnipson <cparsnipson@gmail.com>
2020-01-17 23:20:20 -0800

    Create stub for instruction component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Instruction } from './instruction';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dasm8080-app';
+  
+  instructions: Instruction[] = [
+     { "op": "MOV" },
+     { "op": "SUI" },
+  ];
 }
---
+++ src/app/instruction/instruction.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Instruction } from '../instruction';
+
+@Component({
+  selector: 'app-instruction',
+  templateUrl: './instruction.component.html',
+  styleUrls: ['./instruction.component.scss']
+})
+export class InstructionComponent implements OnInit {
+  @Input()
+  inst : Instruction;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
