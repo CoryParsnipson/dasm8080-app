f590d2993b2793bdaf9d964d74c4acc2fe84c5bb
Cory Parsnipson <cparsnipson@gmail.com>
2020-02-01 11:06:11 -0800

    Some small formatting changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dasm8080-app';
+  title = 'Online Intel 8080 Disassembler';
 }
