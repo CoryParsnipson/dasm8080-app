4730ac62c693f9a985ffe77fa0ee9ba941db6186
Cory Parsnipson <cparsnipson@gmail.com>
2020-02-08 00:16:31 -0800

    Install Clarity Angular Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { ClarityModule } from '@clr/angular';
 
 import { AppComponent } from './app.component';
 import { InstructionListModule } from './instruction-list/instruction-list.module';
@@ -12,6 +13,7 @@ import { UploadModule } from './upload/upload.module';
   ],
   imports: [
      BrowserModule,
+     ClarityModule,
      FormsModule,
      InstructionListModule,
      UploadModule,
