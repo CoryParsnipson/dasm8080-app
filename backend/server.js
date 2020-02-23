f8436e1a42f65da6ee61df0294d197d7911e0d9a
Cory Parsnipson <cparsnipson@gmail.com>
2020-02-22 23:53:27 -0800

    Add AWS amplify backend config. Let's see how deploying to AWS goes.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const cors = require('cors')
 const express = require('express')
 const upload = require('./upload')
+const serverless = require('serverless-http');
 
 const server = express()
 
@@ -12,6 +13,7 @@ server.use(cors(corsOptions))
 
 server.post('/upload', upload)
 
-server.listen(8000, '0.0.0.0', () => {
-   console.log("Server started!")
-})
+//server.listen(8000, '0.0.0.0', () => {
+//   console.log("Server started!")
+//})
+module.exports.handler = serverless(server);
