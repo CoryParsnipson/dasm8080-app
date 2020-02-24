33cd524ee632a8d7cedfc5e6e55c016ae6f9626a
Cory Parsnipson <cparsnipson@gmail.com>
2020-02-23 21:46:59 -0800

    Remove serverless modifications to backend/server.js.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const cors = require('cors')
 const express = require('express')
 const upload = require('./upload')
-const serverless = require('serverless-http');
 
 const server = express()
 
@@ -13,7 +12,6 @@ server.use(cors(corsOptions))
 
 server.post('/upload', upload)
 
-//server.listen(8000, '0.0.0.0', () => {
-//   console.log("Server started!")
-//})
-module.exports.handler = serverless(server);
+server.listen(8000, '0.0.0.0', () => {
+   console.log("Server started!")
+})
