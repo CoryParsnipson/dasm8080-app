63993c24e34cac6bfc1bbe786b94f7d08f8a2f9b
Cory Parsnipson <cparsnipson@gmail.com>
2020-02-26 00:16:51 -0800

    Remove all the AWS stuff.
    Let's deploy on Amazon EC2 instead. It's way more straightforward.
    This whole AWS Amplify thing is a boondoggle.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { ClarityModule } from '@clr/angular';
-import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
 
 import { AppComponent } from './app.component';
 import { InstructionListModule } from './instruction-list/instruction-list.module';
@@ -20,11 +19,8 @@ import { ThemeSwitcherComponent } from './theme-switcher/theme-switcher.componen
      FormsModule,
      InstructionListModule,
      UploadModule,
-     AmplifyAngularModule,
   ],
-  providers: [
-     AmplifyService
-  ],
+  providers: [],
   bootstrap: [
      AppComponent
   ]
